578db26fbf6cb832a498a7077b24e3e278bb77a4
vcheungg <vivianncheungg@gmail.com>
2025-05-05 16:51:40 -0700

    added button hover color/javascript ai prompt/context/function

--- src/script.js
+++ src/script.js
@@ -9,10 +9,11 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-
+  let userInput = document.querySelector("#user-idea");
   let apiKey = "047t2173e3a39c66942c701baf3a6of5";
-  let prompt = "";
-  let context = "";
+  let prompt = `User inputs: Generate a haiku about ${userInput.value}`;
+  let context =
+    "You are a funny haiku expert. Make sure to follow user inputs and separate each line with a <br />";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
