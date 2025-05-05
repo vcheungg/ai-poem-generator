7d680beb2eb483d14bed71ba2d9a9da056c12552
vcheungg <vivianncheungg@gmail.com>
2025-05-05 16:30:20 -0700

    added axios and poem generator functions

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The poem here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "047t2173e3a39c66942c701baf3a6of5";
+  let prompt = "";
+  let context = "";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
